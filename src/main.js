0f16c2a8e181ae495735fbc0120b72d18f99847e
nmskm240 <bokaronogod@gmail.com>
2022-09-11 12:55:30 +0900

    google cloud functionalization

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { Wallet } from "./wallet.js";
 import * as MF from "./moneyforward.js";
 import * as dotenv from "dotenv";
+import functions from'@google-cloud/functions-framework';
 
 dotenv.config();
 
-const wallet = await (await Wallet.load()).toJPYPrice();
-await MF.login();
-await MF.updateWallet("Binance", wallet);
-await MF.logout();
+functions.http('synchronize', async (req, res) =>  {
+    const wallet = await (await Wallet.load()).toJPYPrice();
+    await MF.login();
+    await MF.updateWallet("Binance", wallet);
+    await MF.logout();
+    res.send("complite!");
+});
+
